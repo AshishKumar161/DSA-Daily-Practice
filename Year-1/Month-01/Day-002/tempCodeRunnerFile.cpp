8456a33799a1058efce7affe3aea2149f7ec5623
AshishKumar161 <akumarpandit161@outlook.com>
2026-05-24 09:52:21 +0530

    Remove exe files and add gitignore

---
+++ Year-1/Month-01/Day-002/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+     int arr[100] , n ;
+     
+     cout << "Enter the size of array: " ;
+     cin  >>  n ;
+
+     cout << "Enter the element of array " "\n" ;
+     for (int  i = 0; i < n; i++)
+     {
+        cin >> arr[i] ;
+     }
+
+     cout << "Array " << "\n" ;
+     for (int  i = 0; i < n; i++)
+     {
+        cout << arr[i] << " " ;
+     }
