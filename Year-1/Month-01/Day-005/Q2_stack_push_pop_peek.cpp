a02f914128105c2269f5504f588c28f7cbe65772
AshishKumar161 <akumarpandit161@outlook.com>
2026-05-27 10:39:32 +0530

    Day 006: Add stack practice questions

---
+++ Year-1/Month-01/Day-005/Q2_stack_push_pop_peek.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+    int top;
+    int arr[100] ;
+
+public:
+    Stack()
+    {
+        top = -1 ;
+    }
+
+    void push(int val)
+    {
+        if (top == 99)
+        {
+            cout << "Stack overflow" << "\n";
+        }
+        else
+        {
+            top++;
+            arr[top] = val;
+        }
+    }
+
+    void pop()
+    {
+        if (top == -1)
+        {
+            cout << "Stack underflow" << "\n" ;
+        }
+        else
+        {
+            top--;
+        }
+    }
+
+    void peek()
+    {
+        if (top == -1)
+        {
+            cout << "Stack is empty" << "\n" ;
+        }
+        else
+        {
+            cout << "Top element: " << arr[top] << "\n" ;
+        }
+    }
+
+    void display()
+    {
+        if (top == -1)
+        {
+            cout << "Stack is empty" << "\n" ;
+        }
+        else
+        {
+            for (int i = top; i >= 0; i--)
+            {
+                cout << arr[i] << " " ;
+            }
+            cout << "\n" ;
+        }
+    }
+
+
+};
+
+int main()
+{
+    system("cls");
+
+    Stack s;   
+    s.push(10) ;
+    s.push(20) ;
+    s.push(30) ;
+    s.display() ;
+    s.peek() ;
+    s.pop() ;
+    s.display() ;
+
+
+    system("pause");
+    system("cls");
+    return 0;
+}
