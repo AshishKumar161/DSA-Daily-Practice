cfccd096eb053eb5b7d67d1be3520345f28b3d8b
AshishKumar161 <akumarpandit161@outlook.com>
2026-05-28 11:19:20 +0530

    Day 006: Add stack application questions

--- Year-1/Month-01/Day-006/Q1_balanced_parentheses.cpp
+++ Year-1/Month-01/Day-006/Q1_balanced_parentheses.cpp
@@ -1,1 +1,100 @@
+#include <iostream>
+using namespace std;
 
+#define SIZE 100
+
+class Stack
+{
+    char arr[SIZE] ;
+    int top ;
+
+public:
+    Stack()
+    {
+        top = -1 ;
+    }
+
+    void push(char ch)
+    {
+        if (top == SIZE - 1)
+        {
+            cout << "Stack Overflow" ;
+        }
+        else
+        {
+            top++ ; 
+            arr[top] = ch ;
+        }
+    }
+
+    char pop()
+    {
+        if (top == -1)
+        {
+            return '\0' ;
+        }
+        else
+        {
+            char ch = arr[top] ;
+            top-- ;
+            return ch ; 
+        }
+    }
+
+    bool isEmpty()
+    {
+        return top == -1 ;
+    }
+};
+
+bool isBalanced(string s)
+{
+    Stack st ;
+
+    for (int  i = 0; i < s.length(); i++)
+    {
+        char ch = s[i] ;
+
+        if (ch == '(' || ch == '{' || ch == '[')
+        {
+            st.push(ch) ;
+        }
+        else if (ch == ')' || ch == '}' || ch == ']')
+        {
+            if (st.isEmpty())
+            {
+                return  false ; 
+            }
+
+            char top = st.pop() ;
+
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '['))
+            {
+                return false ;
+            }
+        }
+    }
+
+    return st.isEmpty() ;
+}
+
+int main()
+{
+    string brackets;
+
+    cout << "Enter brackets: " ;
+    cin >>  brackets ;
+
+    if (isBalanced(brackets))
+    {
+        cout << "Balanced Parentheses" ;
+    }
+    else
+    {
+        cout << "Not Balanced Parentheses" ;
+    }
+
+    return 0;
+}
