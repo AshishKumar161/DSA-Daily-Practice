3f0af234cb09b3a59239bbdc18136d5b590d7ac6
AshishKumar161 <akumarpandit161@outlook.com>
2026-06-01 06:59:59 +0530

    Day 009: Add singly linked list basics

---
+++ Year-1/Month-01/Q1_create_and_display_linked_list.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+void display(Node* head) {
+    Node* temp = head;
+
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+
+    cout << "NULL" << endl;
+}
+
+int main() {
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+
+    head->next = second;
+    second->next = third;
+
+    cout << "Linked List:" << endl;
+    display(head);
+
+    return 0;
+}
