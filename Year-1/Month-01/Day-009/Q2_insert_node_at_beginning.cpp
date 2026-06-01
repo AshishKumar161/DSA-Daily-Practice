d3080e520656ae863c48158484a40b8edb4c6aba
AshishKumar161 <akumarpandit161@outlook.com>
2026-06-01 07:45:43 +0530

    Day 009: Add singly linked list basics

--- Year-1/Month-01/Day-009/Q2_insert_node_at_beginning.cpp
+++ Year-1/Month-01/Day-009/Q2_insert_node_at_beginning.cpp
@@ -1,50 +1,55 @@
 #include <iostream>
 using namespace std;
 
-class Node {
+class Node 
+{
 public:
-    int data;
-    Node* next;
+    int data ;
+    Node* next ;
 
-    Node(int value) {
-        data = value;
-        next = NULL;
+    Node(int value) 
+    {
+        data = value ;
+        next = NULL ;
     }
 };
 
-void push_Front(Node*& head, int value) {
-    Node* newNode = new Node(value);
+void push_Front(Node*& head, int value) 
+{
+    Node* newNode = new Node(value) ;
 
-    newNode->next = head;
-    head = newNode;
+    newNode->next = head ;
+    head = newNode ;
 }
 
-void display(Node* head) {
-    Node* temp = head;
+void display(Node* head) 
+{
+    Node* temp = head ;
 
-    while (temp != NULL) {
-        cout << temp->data << " -> ";
-        temp = temp->next;
+    while (temp != NULL) 
+    {
+        cout << temp->data << " -> " ;
+        temp = temp->next ;
     }
 
-    cout << "NULL" << endl;
+    cout << "NULL" << "\n" ;
 }
 
 int main() {
     system("cls");
 
-    Node* head = new Node(20);
-    Node* second = new Node(30);
+    Node* head = new Node(20) ;
+    Node* second = new Node(30) ;
 
-    head->next = second;
+    head->next = second ;
 
-    cout << "Original List:" << endl;
-    display(head);
+    cout << "Original List:" << "\n" ;
+    display(head) ;
 
-    push_Front(head, 10);
+    push_Front(head, 10) ;
 
-    cout << "\nAfter inserting 10 at beginning:" << endl;
-    display(head);
+    cout << "\nAfter inserting 10 at beginning:" << "\n" ;
+    display(head) ;
 
     system("pause");
     system("cls");
