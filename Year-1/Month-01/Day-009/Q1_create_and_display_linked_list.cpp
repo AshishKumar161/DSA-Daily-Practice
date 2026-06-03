f1109cf320f66aa7063da261c235ee3f4d634c1a
AshishKumar161 <akumarpandit161@outlook.com>
2026-06-03 23:58:59 +0530

    Day 011: Add linked list deletion questions

--- Year-1/Month-01/Day-009/Q1_create_and_display_linked_list.cpp
+++ Year-1/Month-01/Day-009/Q1_create_and_display_linked_list.cpp
@@ -1,46 +1,118 @@
+// #include <iostream>
+// using namespace std;
+
+// class Node
+// {
+// public:
+//     int data ;
+//     Node* next ;
+
+//     Node(int value)
+//     {
+//         data = value ;
+//         next = NULL ;
+//     }
+// };
+
+// void display(Node* head)
+// {
+//     Node* temp = head ;
+
+//     while (temp != NULL)
+//     {
+//         cout << temp->data << " -> " ;
+//         temp = temp->next ;
+//     }
+
+//     cout << "NULL" << "\n" ;
+// }
+
+// int main() {
+//     system("cls");
+
+//     Node* head = new Node(10) ;
+//     Node* second = new Node(20) ;
+//     Node* third = new Node(30) ;
+
+//     head->next = second ;
+//     second->next = third ;
+
+//     cout << "Linked List:" << "\n" ;
+//     display(head) ;
+
+//     system("pause");
+//     system("cls");
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-
-class Node 
+class Node
 {
 public:
-    int data ;
-    Node* next ;
+    int data;
+    Node *next;
 
-    Node(int value) 
+    Node(int data)
     {
-        data = value ;
-        next = NULL ;
+        this->data = data;
+        this->next = NULL;
     }
 };
 
-void display(Node* head) 
+class list
 {
-    Node* temp = head ;
+public:
+    Node *head;
 
-    while (temp != NULL)
+    list()
     {
-        cout << temp->data << " -> " ;
-        temp = temp->next ;
+        head = NULL;
     }
 
-    cout << "NULL" << "\n" ;
-}
+    void push(int val)
+    {
+        Node *newNode = new Node(val);
 
-int main() {
+        if (head == NULL)
+        {
+            head = newNode;
+            return;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void print()
+    {
+        Node *temp;
+        temp = head;
+
+        while (temp != NULL)
+        {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL";
+    }
+};
+
+int main()
+{
     system("cls");
 
-    Node* head = new Node(10) ;
-    Node* second = new Node(20) ;
-    Node* third = new Node(30) ;
+    list ll;
+    ll.push(55);
+    ll.push(25);
+    ll.push(555);
 
-    head->next = second ;
-    second->next = third ;
-
-    cout << "Linked List:" << "\n" ;
-    display(head) ;
+    ll.print();
 
     system("pause");
     system("cls");
     return 0;
 }
+
