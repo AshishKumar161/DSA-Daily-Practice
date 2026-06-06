50cd41d17aade60d4a8f7f578c6e4b259535ef7a
AshishKumar161 <akumarpandit161@outlook.com>
2026-06-06 21:34:35 +0530

    Day 014: Add linked list

---
+++ Year-1/Month-01/Day-014/Q1_find_middle_node.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class List
+{
+public:
+    Node *head;
+    Node *tail;
+
+    List()
+    {
+        head = tail = NULL;
+    }
+
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+
+        cout << "NULL" << endl;
+    }
+
+    void find_middle()
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty" << endl;
+            return;
+        }
+
+        Node *slow = head;
+        Node *fast = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        cout << "Middle node = " << slow->data << endl;
+    }
+};
+
+int main()
+{
+    List l;
+
+    l.push_back(10);
+    l.push_back(20);
+    l.push_back(30);
+    l.push_back(40);
+    l.push_back(50);
+
+    cout << "Linked List:" << endl;
+    l.display();
+
+    l.find_middle();
+
+    return 0;
+}
