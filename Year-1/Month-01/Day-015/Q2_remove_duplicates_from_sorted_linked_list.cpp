356139d93404a773561d7351b1794a8d7da705e0
AshishKumar161 <akumarpandit161@outlook.com>
2026-06-07 21:26:18 +0530

    Day 015: Add linked list loop and duplicate questions

---
+++ Year-1/Month-01/Day-015/Q2_remove_duplicates_from_sorted_linked_list.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class List
+{
+public:
+    Node *head;
+    Node *tail;
+
+    List()
+    {
+        head = tail = NULL;
+    }
+
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    void removeDuplicates()
+    {
+        Node *current = head;
+
+        while (current != NULL && current->next != NULL)
+        {
+            if (current->data == current->next->data)
+            {
+                Node *duplicate = current->next;
+                current->next = current->next->next;
+
+                if (duplicate == tail)
+                {
+                    tail = current;
+                }
+
+                delete duplicate;
+            }
+            else
+            {
+                current = current->next;
+            }
+        }
+    }
+
+    void display()
+    {
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+
+        cout << "NULL" << endl;
+    }
+};
+
+int main()
+{
+    List l;
+
+    l.push_back(10);
+    l.push_back(20);
+    l.push_back(20);
+    l.push_back(30);
+    l.push_back(30);
+    l.push_back(40);
+
+    cout << "Original List:" << endl;
+    l.display();
+
+    l.removeDuplicates();
+
+    cout << "After removing duplicates:" << endl;
+    l.display();
+
+    return 0;
+}
