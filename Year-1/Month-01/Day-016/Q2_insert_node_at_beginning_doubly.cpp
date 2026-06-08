38bf6f7cec7c997a24044e023c512b95a7cec97e
AshishKumar161 <akumarpandit161@outlook.com>
2026-06-08 10:32:35 +0530

    Day 016: Add doubly linked list basics

---
+++ Year-1/Month-01/Day-016/Q2_insert_node_at_beginning_doubly.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    Node *prev;
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->prev = NULL;
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class DoublyList
+{
+public:
+    Node *head;
+    Node *tail;
+
+    DoublyList()
+    {
+        head = tail = NULL;
+    }
+
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+
+    void insertAtBeginning(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+
+        cout << "NULL <- ";
+
+        while (temp != NULL)
+        {
+            cout << temp->data;
+
+            if (temp->next != NULL)
+            {
+                cout << " <-> ";
+            }
+
+            temp = temp->next;
+        }
+
+        cout << " -> NULL" << endl;
+    }
+};
+
+int main()
+{
+    DoublyList dl;
+
+    dl.push_back(20);
+    dl.push_back(30);
+
+    cout << "Original List:" << endl;
+    dl.display();
+
+    dl.insertAtBeginning(10);
+
+    cout << "After inserting 10 at beginning:" << endl;
+    dl.display();
+
+    return 0;
+}
