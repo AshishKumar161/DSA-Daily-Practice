f8512af99ef4d651f42feec4b310e21fdfa8c5ea
AshishKumar161 <akumarpandit161@outlook.com>
2026-06-11 11:13:52 +0530

    update

--- Year-1/Month-01/Day-013/Q1_count_nodes_in_linked_list.cpp
+++ Year-1/Month-01/Day-013/Q1_count_nodes_in_linked_list.cpp
@@ -1,60 +1,58 @@
-
-
 #include <iostream>
 using namespace std;
 
 class Node
 {
 public:
-    int data;
-    Node *next;
+    int data ;
+    Node* next ; 
 
     Node(int data)
     {
-        this->data = data;
-        this->next = NULL;
+        this -> data = data ;
+        this -> next = NULL ;
     }
 };
 
 class list
 {
 public:
-    Node *head;
-    Node *tail;
+    Node* head ;
+    Node* tail ;
 
     list()
     {
-        head = tail = NULL;
+        head = tail = NULL ;
     }
 
     void push_front(int val)
     {
-        Node *newNode = new Node(val);
+        Node* newNode = new Node(val);
 
         if (head == NULL)
         {
-            head = tail = newNode ;
-            return;
+            head = tail = newNode  ;
+            return ;
         }
 
         else
         {
-            newNode->next = head;
-            head = newNode;
+            newNode->next = head ;
+            head = newNode ;
         }
     }
 
     int count_node()
     {
-        int count = 0;
-        Node *temp;
-        temp = head;
+        int count = 0 ;
+        Node* temp ;
+        temp = head ;
         while (temp != NULL)
         {
-            count++;
-            temp = temp->next;
+            count++ ;
+            temp = temp -> next ;
         }
-        return count;
+        return count ;
     }
 
     void display()
