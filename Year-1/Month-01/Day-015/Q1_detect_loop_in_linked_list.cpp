b9ce6c802d907d41b8804af59b7c2591ac61247d
AshishKumar161 <akumarpandit161@outlook.com>
2026-06-12 11:07:53 +0530

    update

--- Year-1/Month-01/Day-015/Q1_detect_loop_in_linked_list.cpp
+++ Year-1/Month-01/Day-015/Q1_detect_loop_in_linked_list.cpp
@@ -1,64 +1,70 @@
+
 #include <iostream>
 using namespace std;
 
-class Node
+class Node 
 {
-public:
-    int data;
-    Node *next;
+    public:
+    int data ;
+    Node* next ;
 
     Node(int data)
     {
-        this->data = data;
-        this->next = NULL;
+        this -> data = data ;
+        this -> next = NULL ;
     }
+    
 };
 
-class List
+class list 
 {
-public:
-    Node *head;
-    Node *tail;
+    public:
+    Node* head ;
+    Node* tail ;
 
-    List()
+    list()
     {
-        head = tail = NULL;
+        head = tail =  NULL ;
     }
 
-    void push_back(int val)
+    void push_front(int val)
     {
-        Node *newNode = new Node(val);
+        Node* newNode = new Node(val) ;
 
         if (head == NULL)
         {
-            head = tail = newNode;
-            return;
+            head = tail = newNode ;
+            return ;
         }
-
-        tail->next = newNode;
-        tail = newNode;
+        
+        else
+        {
+            newNode -> next = head ;
+            head = newNode ;
+        }
+        
     }
 
-    bool detectLoop()
+    bool detect_loop()
     {
-        Node *slow = head;
-        Node *fast = head;
+        Node* slow = head ;
+        Node* fast = head ;
 
-        while (fast != NULL && fast->next != NULL)
+        while (fast != NULL && fast -> next != NULL)
         {
-            slow = slow->next;
-            fast = fast->next->next;
+            slow = slow -> next ;
+            fast = fast -> next -> next ;
 
-            if (slow == fast)
+            if (slow == fast )
             {
-                return true;
+                return true ;
             }
         }
-
-        return false;
+        
+        return false ;
     }
 
-    void createLoop()
+        void create_loop()
     {
         // Creating loop:
         // 10 -> 20 -> 30 -> 40
@@ -67,32 +73,32 @@ public:
 
         tail->next = head->next;
     }
+
 };
 
-int main()
-{
-    system("cls");
+int main() {
+     system("cls");
 
-    List l;
+     list ll;
 
-    l.push_back(10);
-    l.push_back(20);
-    l.push_back(30);
-    l.push_back(40);
+     ll.push_front(1) ;
+     ll.push_front(2) ;
+     ll.push_front(3) ;
+     ll.push_front(4) ;
+    
+     ll.create_loop() ;
 
-    l.createLoop();
+     if (ll.detect_loop())
+     {
+         cout << "Loop detected" << "\n" ;
+     }
 
-    if (l.detectLoop())
-    {
-        cout << "Loop detected" << endl;
-    }
-    else
-    {
-        cout << "No loop detected" << endl;
-    }
+     else
+     {
+         cout << "No loop detected" << "\n" ;
+     }
 
-    system("pause");
-    system("cls");
-    return 0;
+     system("pause");
+     system("cls");
+     return 0;
 }
-
