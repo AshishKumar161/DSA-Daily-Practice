5d1f9bce44cff84c6008fb9d5ad7cc2a8c66420a
AshishKumar161 <akumarpandit161@outlook.com>
2026-06-16 11:59:21 +0530

    Day 023: Add recursion on digits questions

---
+++ Year-1/Month-01/DAY-022/Q2_sum_of_digits_using_recursion.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int recursion(int n)
+{
+    if (n == 0)
+    {
+        return 0 ;
+    }
+
+    else
+    {
+        recursion (n % 10) + recursion (n/10) ;
+    }
+    
+    
+}
+
+int main() {
+     system("cls");
+    
+     int n ;
+     cout << "Enter the Number: " ;
+     cin >> n ;
+
+     cout << "Sum of digits = " << recursion(n) << "\n" ;
+    
+     system("pause");
+     system("cls");
+     return 0;
+}
---
+++ Year-1/Month-01/DAY-022/README.md
@@ -0,0 +1,14 @@
+# Day 023
+
+## Questions Solved
+
+1. Count Digits of a Number Using Recursion
+2. Sum of Digits of a Number Using Recursion
+
+## Topic
+
+Recursion
+
+## Language
+
+C++
